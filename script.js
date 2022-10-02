df135a9b98d1de9af2715adf4d96d4d97ddc53fa
faraz <farazzy4u@gmail.com>
2022-10-02 09:59:10 -0500

    initial commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const loader = document.getElementById('loader');
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
-let photosArray = [];
+let photosArray = []; 
 
 // Unsplash API 
 let count = 5;
